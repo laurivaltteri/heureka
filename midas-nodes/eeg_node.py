7a2ad390d124a284113d4a420b63e78669e4a44e
laurivaltteri <llho@ttl.fi>
2018-01-17 10:55:56 +0200

    channel names changed

--- midas-nodes/eeg_node.py
+++ midas-nodes/eeg_node.py
@@ -13,30 +13,15 @@ class EEGNode(BaseNode):
     def __init__(self, *args):
         """ Initialize EEG node. """
         super().__init__(*args)
-        self.metric_functions.append(self.fp1beta)
-        self.metric_functions.append(self.fp2beta)
+        self.metric_functions.append(self.betapower)
 
     def fp1beta(self, x):
-        """ Calculates beta power for channel 1.
-            Note: Function assumes the first channel is Fp1 and the second Fp2.
-            Implementation based on:
-                XXX
+        """ Calculates beta power for input channel.
         """
         f, P = scipy.signal.welch(x['data'][0], fs=self.sampling_rate)
         p_fp1_beta = np.mean(P[np.bitwise_and(f >= 12.0, f <= 25.0)])
 
-        return p_fp1_beta
-
-    def fp2beta(self, x):
-        """ Calculates beta power for channel 2.
-            Note: Function assumes the first channel is Fp1 and the second Fp2.
-            Implementation based on:
-                XXX
-        """
-        f, P = scipy.signal.welch(x['data'][1], fs=self.sampling_rate)
-        p_fp2_beta = np.mean(P[np.bitwise_and(f >= 12.0, f <= 25.0)])
-
-        return p_fp2_beta
+        return betapower
 
 # Run the node from command line
 if __name__ == '__main__':
